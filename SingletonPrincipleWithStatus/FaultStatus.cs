bbe0e03258b5c415fd44cf2ebcfc9de663854226
ITKwiatek <ITKwiatek@protonmail.com>
2021-10-15 20:04:08 +0200

    Add project files.

---
+++ SingletonPrincipleWithStatus/FaultStatus.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPrincipleWithStatus
+{
+    class FaultStatus
+    {
+        private static readonly FaultStatus _instance = new FaultStatus();
+        private bool Status1;
+        private bool Status2;
+
+        public void SetStatusses(bool Status1, bool Status2)
+        {
+            this.Status1 = Status1;
+            this.Status2 = Status2;
+        }
+
+        public bool IsAllTrue()
+        {
+            if(Status1 && Status2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool GetStatus1()
+        {
+            return Status1;
+        }
+
+        public void SetStatus1(bool status)
+        {
+            Status1 = status;
+        }
+
+        public bool GetStatus2()
+        {
+            return Status2;
+        }
+
+        public void SetStatus2(bool status)
+        {
+            Status2 = status;
+        }
+
+        public static FaultStatus GetFaultStatus()
+        {
+            return _instance;
+        }
+    }
+}
---
+++ SingletonPrincipleWithStatus/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SingletonPrincipleWithStatus
+{
+    class Program
+    {
+        static FaultStatus faultStatus = FaultStatus.GetFaultStatus();
+        static void Main(string[] args)
+        {
+            faultStatus.SetStatusses(true, false);
+
+            Console.WriteLine($"--> Status1: {faultStatus.GetStatus1()}");
+            Console.WriteLine($"--> Status2: {faultStatus.GetStatus2()}");
+
+            if (faultStatus.IsAllTrue())
+            {
+                //Action
+            }
+
+            var test = new TestSingleton();
+        }
+    }
+
+    class TestSingleton
+    {
+        static FaultStatus faultStatusTest = FaultStatus.GetFaultStatus();
+        public TestSingleton()
+        {
+            Console.WriteLine("--> Testing class:");
+            Console.WriteLine($"--> Status1: {faultStatusTest.GetStatus1()}");
+            Console.WriteLine($"--> Status2: {faultStatusTest.GetStatus2()}");
+        }
+    }
+}
